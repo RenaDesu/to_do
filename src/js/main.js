c70da30480fba137fa538b5a6b4be073ec8730b0
Elena Voloshina <e.s.voloshina@mail.ru>
2023-09-05 13:51:37 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Vendors
+ */
+// Пример подключения плагина
+// import pluginName from 'plugin-name';
+
+
+
+/**
+ * Modules
+ */
+// Пример подключения модуля
+// import myModule from './modules/my-module-function';
+import '../index.html';
+import '../assets/scss/style.scss';
