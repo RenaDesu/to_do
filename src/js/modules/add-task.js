58623b632b72d19527c8f00e97f0809a0d069f4d
Elena Voloshina <e.s.voloshina@mail.ru>
2023-09-06 11:20:55 +0300

    Добавляет сортировку задач по дате

--- src/js/modules/add-task.js
+++ src/js/modules/add-task.js
@@ -3,11 +3,12 @@ const taskTemplate = document.querySelector('#task-template').content
     .querySelector('.tasks__item');
 
 
-export function addTask(taskName, taskDescr, taskDate, taskTime) {
+export function addTask(taskName, taskDescr, taskDate, taskTime, id) {
     const taskElement = taskTemplate.cloneNode(true);
     taskElement.querySelector('.tasks__title').innerText = taskName;
     taskElement.querySelector('.tasks__description').innerText = taskDescr;
     taskElement.querySelector('.tasks__date').innerText = taskDate;
     taskElement.querySelector('.tasks__time').innerText = taskTime;
+    taskElement.setAttribute('id', `${id}`);
     taskContainer.appendChild(taskElement);
 }
--- src/js/modules/localstorage.js
+++ src/js/modules/localstorage.js
@@ -8,6 +8,8 @@ const taskDate = document.querySelector('[data-task-date]');
 const taskTime = document.querySelector('[data-task-time]');
 const taskAddBtn = document.querySelector('[data-task-add]');
 const form = document.querySelector('[data-form]');
+const sortBtn = document.querySelector('[data-sort]');
+const taskContainer = document.querySelector('[data-task-container]');
 
 taskAddBtn.addEventListener('click', () => {
     getTask(taskName.value, taskDescription.value, taskDate.value, taskTime.value);
@@ -26,15 +28,51 @@ function getTask(name, description, date, time) {
     }, ];
 
     localStorage.setItem(`item ${localStorage.length}`, JSON.stringify(tasksArray[0]));
-   
+
     data = JSON.parse(localStorage.getItem(`item ${localStorage.length - 1}`));
 
-    addTask(data.name, data.description, data.date, data.time);
+    addTask(data.name, data.description, data.date, data.time, localStorage.length - 1);
 }
 
 for (let i = 0; i < localStorage.length; i++) {
     let data;
     data = JSON.parse(localStorage.getItem(`item ${i}`));
-    addTask(data.name, data.description, data.date, data.time);
+    addTask(data.name, data.description, data.date, data.time, i);
 }
+
+sortBtn.addEventListener('click', () => {
+    let dataArr = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        let data;
+        data = JSON.parse(localStorage.getItem(`item ${i}`));
+        dataArr.push(data);
+    }
+    const sortedDataArr = dataArr.sort((x, y) => {
+        if (x.date < y.date) {
+            return -1;
+        }
+        if (x.date > y.date) {
+            return 1;
+        }
+
+        if (x.time < y.time) {
+            return -1;
+        }
+        if (x.time > y.time) {
+            return 1;
+        }
+        return 0;
+    });
+
+    taskContainer.innerHTML = 
+    `<li class="tasks__main-item">
+        <h3 class="tasks__title">Задачи</h3>
+        <button class="button button--small" data-sort type="button">Сортировать по дате</button>
+     </li>`;
+
+    sortedDataArr.forEach((elem, i) => {
+        addTask(elem.name, elem.description, elem.date, elem.time, i);
+    });
+
+});
 // localStorage.clear();
